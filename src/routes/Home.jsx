529899ac1dbe96843d3fc5280e45d94bb0a70040
saidulislam28 <saidulislam79028@gmail.com>
2024-05-11 02:54:10 +0600

    login and register design done

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,14 @@
+import Banner from "../components/Banner";
 import Faq from "../components/Faq";
 import Feature from "../components/Feature";
 
 const Home = () => {
   return (
     <div>
+
+      <Banner></Banner>
+
+
       <Feature></Feature>
 
       <Faq></Faq>
--- src/routes/authentication/Register.jsx
+++ src/routes/authentication/Register.jsx
@@ -1,9 +1,36 @@
-
+import { Link } from "react-router-dom";
+import "./styles/login.css";
 const Register = () => {
   return (
-  <div>
-    
+    <div className="flex justify-center">
+      <div className="login-container max-w-xl">
+    <div className="heading">Register </div>
+    <form action="" className="form-section">
+
+
+      <input required="" className="input" type="text" name="Name" id="email" placeholder="Name"/>
+
+
+      <input required="" className="input" type="text" name="photo" id="email" placeholder="Photo URL"/>
+
+
+      <input required="" className="input" type="email" name="email" id="email" placeholder="E-mail"/>
+     
+     
+     
+     
+      <input required="" className="input" type="password" name="password" id="password" placeholder="Password"/>
+     
+     
+     
+
+      <input className="login-button" type="submit" value="Sign In"/>
+      
+    </form>
+   
+      <p className="agreement">Already have an account? <Link to="/login" > <span className="underline">Signin</span>  </Link>  </p>
   </div>
+    </div>
   );
 };
 
